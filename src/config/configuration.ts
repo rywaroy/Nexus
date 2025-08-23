47864997437de01d82ace30e913c74c7bc32031d
Otter-BlueJay <464485682@qq.com>
2025-08-23 17:14:48 +0800

    feat: init

---
+++ src/config/configuration.ts
@@ -0,0 +1,23 @@
+export default () => ({
+  app: {
+    port: parseInt(process.env.APP_PORT) || 3000,
+    nodeEnv: process.env.NODE_ENV || 'development',
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'yourSecretKey',
+    expiresIn: process.env.JWT_EXPIRES_IN || '24h',
+  },
+  mongodb: {
+    host: process.env.MONGODB_HOST || '127.0.0.1',
+    port: parseInt(process.env.MONGODB_PORT) || 27017,
+    database: process.env.MONGODB_DB || 'test',
+    user: process.env.MONGODB_USER || '',
+    password: process.env.MONGODB_PASS || '',
+  },
+  redis: {
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: parseInt(process.env.REDIS_PORT) || 6379,
+    password: process.env.REDIS_PASSWORD || '',
+    database: parseInt(process.env.REDIS_DB) || 0,
+  },
+});
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,28 @@
+import {
+    Controller,
+    Post,
+    Body,
+    UseGuards,
+    Request,
+    Get,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '../../common/guards/auth.guard';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @UseGuards(AuthGuard)
+    @Get('info')
+    async getInfo(@Request() req) {
+        console.log(req.user);
+        return req.user;
+    }
+
+    @Post('register')
+    register(@Body() createUserDto: CreateUserDto) {
+        return this.userService.create(createUserDto);
+    }
+}
